53c5c0216ecac181664ff7c6e1217aade5b7efd1
Sebastian-B-Ciubotariu <127081317+Sebastian-B-Ciubotariu@users.noreply.github.com>
2023-03-28 17:05:33 +0300

    Homework_3

---
+++ Homework_3/Car.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Homework_3_Classes
+{
+    public class Car
+    {
+        private int speed;
+        private int gear;
+        private bool engineStarted;
+
+        public Car()
+        {
+            speed = 0;
+            gear = 0;
+            engineStarted = false;
+        }
+
+        public void StartEngine()
+        {
+            engineStarted = true;
+            Console.WriteLine("Engine started.");
+        }
+
+        public void StopEngine()
+        {
+            engineStarted = false;
+            speed = 0;
+            gear = 0;
+            Console.WriteLine("Engine stopped.");
+        }
+
+        public void Accelerate()
+        {
+            if (engineStarted)
+            {
+                speed += 10;
+                gear = speed / 10;
+                Console.WriteLine("Accelerating. Current speed: {0} km/h, current gear: {1}.", speed, gear);
+            }
+            else
+            {
+                Console.WriteLine("The engine is not started.");
+            }
+        }
+
+        public void Brake()
+        {
+            if (engineStarted)
+            {
+                speed -= 10;
+                gear = speed / 10;
+                Console.WriteLine("Decelerating. Current speed: {0} km/h, current gear: {1}.", speed, gear);
+            }
+            else
+            {
+                Console.WriteLine("The engine is not started.");
+            }
+        }
+
+        public void GetCurrentSpeed()
+        {
+            Console.WriteLine("Current speed: {0} km/h.", speed);
+        }
+
+        public void GetCurrentGear()
+        {
+            Console.WriteLine("Current gear: {0}.", gear);
+        }
+    }
+}
---
+++ Homework_3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework_3_Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.StartEngine();    // Engine started.
+            myCar.Accelerate();     // Accelerating. Current speed: 10 km/h, current gear: 1.
+            myCar.Accelerate();     // Accelerating. Current speed: 20 km/h, current gear: 2.
+            myCar.Brake();          // Decelerating. Current speed: 10 km/h, current gear: 1.
+            myCar.Brake();          // Decelerating. Current speed: 0 km/h, current gear: 0.
+            myCar.StopEngine();     // Engine stopped.
+        }
+    }
+}
